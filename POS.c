b2763eee8a36d951068cf432f869092b4680d4cb
Clint <luchavezclintvictor@gmail.com>
2026-08-03 01:04:47 +0800

    Update POS.c

--- POS.c
+++ POS.c
@@ -5,39 +5,45 @@
          int quantity;
          float price;
          char shipping;
-         char food;
+         char Order[10];
          char agree;
-              
-         printf("- POINT OF SALE(POS) TERMINAL - \n\n");
+         char Your_order;
+
+         printf("- POINT OF SALE(POS) TERMINAL - \n\n\n");
 
          // food Options
-         printf("B = Burger\n");
-         printf("F = Fries\n");
-         printf("C = Chicken\n");
-         printf("Enter your food:");
-         scanf(" %c", &food);
+         printf("\t-- Menu: --\n\n");
+         printf("\tBurger\n");
+         printf("\tFries \n");
+         printf("\tChicken\n\n");
+         printf("Enter your order:");
+         scanf(" %s", Order);
+         printf("Your Order is: %s\n", Order);
 
          // for the quantity
-          printf("Enter quatity:");
+          printf("Enter quantity:");
           scanf("%d", &quantity);
+          printf("You Ordered: %d %s\n\n", quantity, Order);
+
 
          // for the price
-          printf("\t-- PRICES --\n");
-          printf("\t Chicken = P150\n");
-          printf("\t Fries = P80\n");
-          printf("\t Burger = P110\n");
+          printf("\t-- PRICES --\n\n");
+          printf("\tChicken = P150\n");
+          printf("\tFries = P80\n");
+          printf("\tBurger = P110\n\n");
           printf("Enter the price of your item:");
           scanf("%f", &price);
-          printf("Type Y to continue:");
+          printf("Your Total is: P%.2f\n", quantity * price);
+          printf("Type C to confirm your order:");
           scanf(" %c", &agree);
 
          // for the shipping
-          printf("Enter shipping option: E for EXPRESS and S for SHIPPING:");
+          printf("Enter shipping option: E for EXPRESS and S for NORMAL SHIPPING:");
           scanf(" %c", &shipping);
 
          // Final receipt
-         printf("\n--- RECEIPT ---\n");
-         printf("Food: %c\n", food);
+         printf("\n\t-- RECEIPT --\n");
+         printf("Order: %s\n", Order);
          printf("Items: %d\n", quantity);
          printf("Price per item: P%.1f\n", price);
          printf("Shipping mode: %c\n", shipping);
